c585afd5ea81084242071d48f29d18863a4efb0c
EhsanTgv <EhsanTgv@gmail.com>
2023-04-13 15:49:57 +0300

    Initial Commit

---
+++ NewImagePickerPractice/NewImagePickerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewImagePickerPracticeApp.swift
+//  NewImagePickerPractice
+//
+//  Created by Ehsan Taghavi on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewImagePickerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
